18ba6e97d21835626d815c835ab22ac3ddc0a88f
Kweku Bright <kweku01bright@gmail.com>
2022-08-24 18:43:07 +0100

    Update decorators based on review

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,10 +1,13 @@
-class Decorator < Nameable
+class Decorator < Nameableat
+  attr_accessor: nameable
+
   def initialize(nameable)
     @nameable = nameable
-    super()
+
   end
 
   def correct_name
     @nameable.correct_name
+
   end
 end
