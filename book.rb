931b0f5bfa1cbf9d5c633f735a0e9cb9bc04933d
Kweku Bright <kweku01bright@gmail.com>
2022-08-26 16:41:26 +0100

    Add accessor for book and person

--- book.rb
+++ book.rb
@@ -10,7 +10,7 @@ class Book
     @rentals = []
   end
 
-  def add_rental
-    @rentals.push(rental)
+  def add_rental(date, person)
+    Rental.new(date, self, person)
   end
 end
--- person.rb
+++ person.rb
@@ -34,8 +34,8 @@ class Person < Nameable
     @name
   end
 
-  def add_rental()
-    @rentals.push(rental)
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private :of_age?
